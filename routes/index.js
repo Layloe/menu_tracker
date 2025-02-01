c1147ea770002652de83453e33350624a89a06a6
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-02-01 08:51:44 -0800

    added controllers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-//add controllers
+const indexController = require('../controllers/index')
 // const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', indexController.getHome)
+router.post('/q', indexController.getSearch)
 
 
 module.exports = router
