a932cfd2b830881719b1a37bba03425185e68896
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2025-02-20 07:18:25 -0800

    search bar query

--- controllers/index.js
+++ controllers/index.js
@@ -9,6 +9,26 @@ module.exports = {
         .then((menuData) => {
             res.render('index', {title: "Menu Tracker", data: menuData, search: '', loggedIn: sesh.loggedIn})
         })
+    },
+    getSearch: async (req, res) => {
+        let menu = schemas.menu;
+        let q = req.body.searchInputs;
+        let menuData = null;
+        let sesh = req.session;
+        let qry = {name:{$regex: '^' + q, $options: 'i'}};
+
+        if (q != null) {
+            let menuResults = await menu.find(qry).then((data) => {
+                menuData = data;
+            });
+        } else {
+            q = 'Search';
+            let menuResult =  await menu.find({}).then( (data) => {
+                menuData = data;
+    });
     }
-    getSearch:
+
+    res.render('index', {title:'Menu App', data:menuData, search:q, loggedIn: sesh.loggedIn});
 }
+
+//! 3:29
